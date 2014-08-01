2571061682e67395413c1c5f79e1cffdde1e9959
Adam Malcontenti-Wilson <adman.com@gmail.com>
2014-08-01 22:41:03 +1000

    initial commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,49 +1,33 @@
-/*!
- *
- *  Web Starter Kit
- *  Copyright 2014 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
-(function () {
+(function($) {
   'use strict';
 
-  var querySelector = document.querySelector.bind(document);
-
-  var navdrawerContainer = querySelector('.navdrawer-container');
-  var body = document.body;
-  var appbarElement = querySelector('.app-bar');
-  var menuBtn = querySelector('.menu');
-  var main = querySelector('main');
-
-  function closeMenu() {
-    body.classList.remove('open');
-    appbarElement.classList.remove('open');
-    navdrawerContainer.classList.remove('open');
-  }
-
-  function toggleMenu() {
-    body.classList.toggle('open');
-    appbarElement.classList.toggle('open');
-    navdrawerContainer.classList.toggle('open');
-  }
-
-  main.addEventListener('click', closeMenu);
-  menuBtn.addEventListener('click', toggleMenu);
-  navdrawerContainer.addEventListener('click', function (event) {
-    if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
-      closeMenu();
-    }
+  var displayPost = function(post, imageUrl) {
+    var $post = $('<div class="post loading"><a href="' + post.url + '"><img src="' + imageUrl + '"><span class="title">' + post.title + '</span></a></div>');
+    $post.imagesLoaded( function() {
+      $post.removeClass('loading');
+    });
+    $post.appendTo('main');
+  };
+  $.getJSON('http://www.reddit.com/r/adviceanimals/hot.json', function(data) {
+    data.data.children.filter(function(post){
+      return post.kind === 't3';
+    }).map(function(data) {
+      return data.data;
+    }).forEach(function(post) {
+      switch(post.domain) {
+        case 'i.imgur.com':
+        case 'imgur.com':
+          var m;
+          if ((m = /^https?:\/\/(?:i\.)?imgur.com\/([^\/]+?)(\..+?)?$/.exec(post.url))) {
+            displayPost(post, 'http://i.imgur.com/' + m[1] + '.png');
+          }
+          break;
+        case 'livememe.com':
+          displayPost(post, post.url);
+          break;
+        default:
+          console.log('unhandled domain:', post.domain, post.url);
+      }
+    });
   });
-})();
+})(window.jQuery);
